36060a3a6e4a5a978aff2e944485e637a6542531
Danial A <danazad2016@gmail.com>
2021-08-04 19:18:44 -0500

    Added an object to store the quantity of products selected

--- store.js
+++ store.js
@@ -19,8 +19,28 @@ productContainer.addEventListener("click", (e) => {
 function productPacker(button) {
   const productContainer = button.closest(".w-full")
 
-  console.log(productContainer)
-
   const product = {}
-  product.name = button.query
+  product.image = productContainer.querySelector("img").src
+  product.name = productContainer.querySelector("h2").textContent
+  product.price = Number(
+    productContainer.querySelector("p").textContent.substring(1)
+  )
+  console.log(product)
+
+  const productColor = [
+    "Red",
+    "Yellow",
+    "Blue",
+    "Orange",
+    "Green",
+    "Purple",
+    "Light Gray",
+    "Dark Gray",
+  ]
+  const productNumbering = {}
+  productColor.forEach((product) => {
+    productNumbering[`${product}`] = 0
+  })
+
+  console.log(productNumbering)
 }
