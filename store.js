fa3d21b7b713a26f24f4d589e1a828432796f8db
Danial A <danazad2016@gmail.com>
2021-08-06 12:10:29 -0500

    Adding functionality to automatically merge colors

--- store.js
+++ store.js
@@ -61,6 +61,13 @@ function cartAdder(product, productNumbering) {
   clone.querySelector(".total-product-price").innerText =
     "$" + product.price + ".00"
   clone.querySelector(".cart-item").classList.add(product.name)
+  if (!product.name === "Dark Gray" || !product.name === "Light Gray") {
+    clone.querySelector(".cart-item").classList.add(product.name)
+  } else if (product.name === "Dark Gray") {
+    product.name === "Dark-Gray"
+  } else if (product.name === "Light Gray") {
+    product.name === "Light-Gray"
+  }
 
   for (const [key, value] of Object.entries(productNumbering)) {
     if (value > 1) {
@@ -76,11 +83,38 @@ function cartAdder(product, productNumbering) {
 
   cartProductList.append(clone)
 
-  const redProducts = Array.from(cartProductList.querySelectorAll(".Red"))
-  const outdatedRedProducts = redProducts.slice(0, -1)
-  outdatedRedProducts.forEach((outdatedRedProduct) => {
-    outdatedRedProduct.remove()
-  })
+  for (const [key, value] of Object.entries(productNumbering)) {
+    if (value > 1) {
+      console.log(key, value)
+
+      const products = Array.from(cartProductList.querySelectorAll(`.${key}`))
+      console.log(products)
+
+      products.forEach((product) => {
+        console.log(product)
+        if (product.querySelector("h2").innerText != value.toString()) {
+          product.remove()
+        }
+      })
+    }
+  }
+
+  // const products = Array.from(cartProductList.querySelectorAll(".cart-item"))
+  // products.forEach(product => {
+  //   if (product.querySelector('h2').innerText)
+  // });
+
+  // const redProducts = Array.from(cartProductList.querySelectorAll(".Red"))
+  // const outdatedRedProducts = redProducts.slice(0, -1)
+  // outdatedRedProducts.forEach((outdatedRedProduct) => {
+  //   outdatedRedProduct.remove()
+  // })
+
+  // const YellowProducts = Array.from(cartProductList.querySelectorAll(".Yellow"))
+  // const outdatedYellowProducts = YellowProducts.slice(0, -1)
+  // outdatedYellowProducts.forEach((outdatedYellowProduct) => {
+  //   outdatedYellowProduct.remove()
+  // })
 }
 
 // Number(
